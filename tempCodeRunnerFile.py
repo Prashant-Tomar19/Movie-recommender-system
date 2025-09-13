9473cb172633c9fc423ecca3b0f3d2d1043f31df
Prashant-Tomar19 <prashanttomar7581@gmail.com>
2025-09-13 11:57:20 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-posters = data.get('posters', [])
-    
-    if posters:
-        # Construct full URLs for posters
-        poster_urls = [
-            "https://image.tmdb.org/t/p/w500" + poster['file_path']
-            for poster in posters
-        ]
